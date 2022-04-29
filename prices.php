00e868e11d8b0402427205ef72ed9fddf6509e43
Umair Abbas <umairabbass786@gmail.com>
2022-04-29 19:41:06 +0500

    prices page reated

---
+++ prices.php
@@ -0,0 +1,86 @@
+<?php include "include/header.php";?>
+<?php include "include/nav.php";?>
+<!-- Inne Page Banner Area Start Here -->
+<section class="inner-page-banner bg-common" data-bg-image="img/figure/breadcumb.jpg">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="breadcrumbs-area">
+                    <h1>Office Cleaning</h1>
+                    <ul>
+                        <li>
+                            <a href="index.php">Home</a>
+                        </li>
+                        <li>Office Cleaning</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Inne Page Banner Area End Here -->
+<!-- Pricing Table Area Start Here -->
+<section class="section-padding-12-9">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p>RG`s Cleaning provides the best commercial office cleaning services for our business clients in Los Angeles area.</p>
+                <p>At RG`s Cleaning we have confidence that our service is among the best. To ensure that we work to the best of our ability, we always consult our cleaning service checklist.</p>
+                <p>Each day we make sure to empty trash bins and replace the bags. We vacuum all carpets thoroughly and we also sweep and mop wood and tile floors. We even sanitize door handles.</p>
+                <div class="heading-layout">
+                    <h2>OFFICE CLEANING</h2>
+                </div>
+                <p>Depending on the number of employees in a business, handles can be touched hundreds of times each day. At the end of each session, we turn off all the lights and lock the doors. We also dust room fixtures once every week.</p>
+                <div class="heading-layout">
+                    <h2>BREAK ROOM CLEANING</h2>
+                </div>
+                <p>Our office cleaning services checklist also has notes for the company break area. The trash bin and floor rules are very much the same, but there are additional items to be considered. For example, break rooms often have refrigerators, coffee makers and microwaves. We will clean the inside of these appliances and sanitize the outside handles to prevent germ transfer or allergic reactions. We also make sure to wipe down countertops, cabinets, tables, chairs as well as the sink and its handles. Every day we also check the roll of paper towels and replace it if necessary. We refill the dish soap weekly and as an added extra service we can offer dish washing. At the end of each day, we turn off all the lights and lock the door to the break room.</p>
+                <div class="heading-layout">
+                    <h2>COMMON AREA CLEANING</h2>
+                </div>
+                <p>We also dedicate a part of our office cleaning services checklist to companies with a common room or social area outside of the standard break room. In addition to the standard trash and floor procedures, we clean the glass doors of common areas each and every day. We will also wipe down any woodwork exposed in the room. After our business is complete, we will turn off all the lights and lock the door.</p>
+                <div class="heading-layout">
+                    <h2>RESTROOM CLEANING</h2>
+                </div>
+                <p>Perhaps our most comprehensive checklist is for the office bathroom. We leave your restrooms sparkling clean. During every session we empty trash and change the bag. We clean and sanitize the toilet seat, handle and bowl. We even scrub and sanitize urinals and their handles. We sanitize the door handles and toilet paper holders, and replace the latter when necessary. Also, we refill soap dispensers and air freshener cartridges as necessary.</p>
+                <h3 class="text-center">RELIABLE AND AFFORDABLE COMMERCIAL</h3>
+                <h3 class="text-center">CLEANING IN LOS ANGELES AREA</h3>
+            </div>
+        </div>
+</section>
+<!-- Pricing Table Area End Here -->
+<!-- Banner Area Start Here -->
+<section class="banner-wrap-layout2 bg-common" data-bg-image="img/figure/banner-bg1.jpg">
+    <div class="container">
+        <div class="row d-flex align-items-center">
+            <div class="col-lg-6 col-12">
+                <div class="banner-box-layout4">
+                    <h2 class="item-title text-Primary">Together <span>We'll Explore</span> New Things</h2>
+                    <a href="quote.php" class="btn-fill-md bg-accent btn-slide-hover text-primarytext">Get an Estimate<i
+                            class="fas fa-angle-right"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-6 col-12">
+                <div class="banner-box-layout5">
+                    <div class="item-icon shape1">
+                        <img src="img/figure/icon-shape4.png" alt="shape">
+                    </div>
+                    <div class="item-icon shape2">
+                        <img src="img/figure/icon-shape3.png" alt="shape">
+                    </div>
+                    <div class="item-icon shape3">
+                        <img src="img/figure/icon-shape2.png" alt="shape">
+                    </div>
+                    <div class="item-icon shape4">
+                        <img src="img/figure/icon-shape1.png" alt="shape">
+                    </div>
+                    <div class="item-img">
+                        <img src="img/figure/banner-figure.png" alt="figure">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Banner Area End Here -->
+<?php include "include/footer.php";?>
